e26ae33c9be1f8f80ab55658ef46bdf874bd3b6c
Kim Wong <wangking@amsay.com>
2025-09-28 13:42:35 +0800

    add search box

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import playformCompress from "@playform/compress";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dvd3.net",
-  integrations: [mdx(), sitemap(), playformCompress()],
+  integrations: [mdx(), sitemap(), playformCompress(),pagefind()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
